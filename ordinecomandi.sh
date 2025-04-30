bfaa2a1e88f86623010420efd0bf4187566fb1bb
crippaemanuele <e.crippa10@studenti.unibg.it>
2025-04-30 09:20:23 +0200

    Aggiunta la visualizzazione della password di accesso a Keycloak nel file ordinecomandi.sh

--- ordinecomandi.sh
+++ ordinecomandi.sh
@@ -10,6 +10,8 @@ kubectl apply -f certs/cluster-issuer.yaml
 kubectl apply -f certs/keycloak/keycloak-certificate.yaml
 #Installazione di Keycloak
 helm upgrade --install keycloak bitnami/keycloak -n keycloak -f keycloak/origin.yaml
+    #Password di accesso a Keycloak
+    kubectl -n keycloak get secret keycloak -o jsonpath='{.data.admin-password}' | base64 -d && echo
 #Installazione di Minio
 helm upgrade --install --namespace minio-operator --create-namespace operator minio-operator/operator
 helm upgrade --install minio minio-operator/tenant --namespace minio-tenant --create-namespace -f old/t-values.yaml
