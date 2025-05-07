1431c78a1d26cc79b258f8f46db894af2b77c074
crippaemanuele <e.crippa10@studenti.unibg.it>
2025-05-07 12:03:09 +0200

    Prova di installazione con vecchi valori per ripartire

--- autorun.sh
+++ autorun.sh
@@ -7,14 +7,14 @@ kubectl create namespace minio-tenant
 #Inizializzazione cert-manager
 helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true
 #Creazione del cluster issuer e dei certificati
-kubectl apply -f certs/cluster-issuer.yaml
-kubectl apply -f certs/keycloak/keycloak-certificate.yaml
-kubectl apply -f certs/minio/minio-api-crt.yaml
-kubectl apply -f certs/minio/minio-console-crt.yaml
+#kubectl apply -f certs/cluster-issuer.yaml
+#kubectl apply -f certs/keycloak/keycloak-certificate.yaml
+#kubectl apply -f certs/minio/minio-api-crt.yaml
+#kubectl apply -f certs/minio/minio-console-crt.yaml
 #Installazione di Keycloak
 #kubectl create configmap keycloak-realm-config --from-file=realm-export.json=/home/lelec/Projects/minio-keycloak-sso/keycloak/realm-export.json -n keycloak
-#helm upgrade --install keycloak bitnami/keycloak -n keycloak -f keycloak/values.yaml
-#kubectl -n keycloak get secret keycloak -o jsonpath='{.data.admin-password}' | base64 -d && echo
+helm upgrade --install keycloak bitnami/keycloak -n keycloak -f keycloak/values.yaml
+kubectl -n keycloak get secret keycloak -o jsonpath='{.data.admin-password}' | base64 -d && echo
 #Installazione di Minio
 helm upgrade --install --namespace minio-operator --create-namespace operator minio-operator/operator
 helm upgrade --install minio minio-operator/tenant --namespace minio-tenant --create-namespace -f old/t-values.yaml
