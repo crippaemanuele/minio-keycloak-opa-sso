41115ff68f5194fd578bfd0ea3e25c8f66384f7a
crippaemanuele <e.crippa10@studenti.unibg.it>
2025-05-12 17:15:11 +0200

    Funge

--- comandi.sh
+++ comandi.sh
@@ -14,4 +14,7 @@ kubectl apply -f certs/tenant-1-ca-issuer.yaml
 kubectl apply -f certs/tenant-1-minio-certificate.yaml
 kubectl get secrets -n tenant-1 tenant-1-ca-tls -o=jsonpath='{.data.ca\.crt}' | base64 -d > ca.crt
 kubectl create secret generic operator-ca-tls-tenant-1 --from-file=ca.crt -n minio-operator
+kubectl apply -f certs/minio-api-crt.yaml
+kubectl apply -f certs/minio-console-crt.yaml
+kubectl create secret generic myminio-tls-custom --from-literal=public.crt="$(kubectl get secret myminio-tls -n tenant-1 -o jsonpath='{.data.tls\.crt}' | base64 -d)" --from-literal=private.key="$(kubectl get secret myminio-tls -n tenant-1 -o jsonpath='{.data.tls\.key}' | base64 -d)" -n tenant-1
 helm upgrade --install myminio minio-operator/tenant --namespace tenant-1 --create-namespace -f minio/t-values.yaml 
