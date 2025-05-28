bbf89a1041fda7dfae21e01e173d759973a22b6b
crippaemanuele <e.crippa10@studenti.unibg.it>
2025-05-28 09:20:28 +0200

    RoleArn Richiesto

--- setup.sh
+++ setup.sh
@@ -18,6 +18,33 @@ fi
 echo "🔗 Creo alias root MinIO..."
 mc alias set minio_root https://minio-api.local minio minio123
 
-# 🐍 Avvia script Python OIDC
-echo "🐍 Avvio autenticazione OIDC tramite Python..."
-python3 oidc_auth.py
+#TO-DO: Da qui in giù riscrivere in python
+#Auth-flow:
+ENDPOINT_KEYCLOAK="https://keycloak.local/realms/MinIO/protocol/openid-connect/token"
+ENDPOINT_MINIO="https://minio-api.local/"
+client_id="minio-client"
+client_secret="UySDgZLFW9GSWjtwVMx4yxEnpMkqD4"
+username="medico"
+password="medico_pass"
+# Ottieni token OIDC
+token_oidc=$(curl -s -d "client_id=$client_id" \
+     -d "client_secret=$client_secret" \
+     -d "grant_type=password" \
+     -d "username=$username" \
+     -d "password=$password" \
+     -d "scope=openid profile email" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -X POST $ENDPOINT_KEYCLOAK | jq -r '.access_token')
+  # Ottieni token STS temporaneo
+sts_response=$(curl -s -X POST "$ENDPOINT_MINIO" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "Action=AssumeRoleWithWebIdentity" \
+  -d "Version=2011-06-15" \
+  -d "WebIdentityToken=$token_oidc" \
+  -d "RoleArn=arn:minio:iam:::role/dummy-internal" \
+  -d "RoleSessionName=my-session")
+  # Accedi a MinIO con token STS
+
+## 🐍 Avvia script Python OIDC
+#echo "🐍 Avvio autenticazione OIDC tramite Python..."
+#python3 oidc_auth.py
